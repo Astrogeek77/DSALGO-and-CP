f286780ba3fcbe6aa857ed004b972b1b65448995
Gautam Jain <astrogeek77@gmail.com>
2021-11-10 17:47:21 +0530

    Added Bubble sort and Insertion sort.

---
+++ Implementations/Algorithms/Sort_Algorithms/insertion_sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int arr[], int n){
+    int j;
+     for(j = 1; j < n; j++){
+        int i = j - 1; 
+        int key = arr[j];
+        
+        while(i >= 0 && arr[i] > key){
+            arr[i + 1] = arr[i];
+            i--;
+            
+        }
+        arr[i + 1] = key;
+        
+        for(int k = 0; k < n; k++) printf("Arr[%d] = %d\t", k, arr[k]);
+        cout << endl;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    // int arr[] = { 4, 3, 2, 10, 12, 1, 5, 6 };
+    // int arr[] = { 10, 20, 30, 40, 50, 60 };
+    int arr[] = { 60, 50, 40, 30, 20, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    insertionSort(arr, n);
+    printArray(arr, n);
+    return 0;
+}
+
+
