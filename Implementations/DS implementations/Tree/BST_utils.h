243661fc1bbf42d99b0a1cfb16db10c14e2db941
Gautam Jain <astrogeek77@outlook.com>
2022-12-22 12:51:49 +0530

    added build tree method from postorder and inorder traversals

--- Implementations/DS implementations/Tree/BST_utils.h
+++ Implementations/DS implementations/Tree/BST_utils.h
@@ -479,6 +479,37 @@ BST *constructTree2(vector<int> &preorder, int &i, int Bound)
     return root;
 }
 
+// Contruct BST from postorder and inorder
+TreeNode* build(vector<int> &inorder, vector<int> &postorder, int &index, int inOrderStart, int inOrderEnd, int n, map< int, int> &nodeToIndex)
+{
+    //base case;
+    if (index < 0 || inOrderStart > inOrderEnd) return NULL;
+
+    int element = postorder[index--];
+    TreeNode *root = new TreeNode(element);
+    int position = nodeToIndex[element];
+
+    root->right = build(inorder, postorder, index, position + 1, inOrderEnd, n, nodeToIndex);
+    root->left = build(inorder, postorder, index, inOrderStart, position - 1, n, nodeToIndex);
+    
+    return root;
+}
+
+TreeNode* buildTree(vector<int> &inorder, vector<int> &postorder)
+{
+    int n = inorder.size();
+    int postOrderIndex = n - 1;
+
+    map<int, int> nodeToIndex;
+    for (int i = 0; i < n; i++)
+    {
+        nodeToIndex[inorder[i]] = i;
+    }
+
+    TreeNode *ans = build(inorder, postorder, postOrderIndex, 0, n - 1, n, nodeToIndex);
+    return ans;
+}
+
 BST *inorderSuccessor1(BST *node, int val)
 {
     if (node == NULL)
