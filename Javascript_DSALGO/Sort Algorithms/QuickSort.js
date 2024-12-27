972fb38c6d12367ea6e91c58e6d12d17fda82b4e
Gautam Jain <astrogeek77@gmail.com>
2024-12-27 18:53:54 +0530

    updated quick sort and radix sort to work with comparator

--- Javascript_DSALGO/Sort Algorithms/QuickSort.js
+++ Javascript_DSALGO/Sort Algorithms/QuickSort.js
@@ -1,28 +1,58 @@
 // Quick sort
-function partition(arr, startIndex = 0, endIndex = arr.length - 1){
+function partition(arr, startIndex = 0, endIndex = arr.length - 1) {
   const swap = (arr, idx1, idx2) => {
-      [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]
-  }
-  
-  let pivot = arr[startIndex], pvtIdx = startIndex
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
 
-  for(let i = startIndex+1; i <= endIndex; i++){
-    if(pivot > arr[i]){
-      pvtIdx++
-      swap(arr, pvtIdx, i)
+  let pivot = arr[startIndex],
+    pvtIdx = startIndex;
+
+  for (let i = startIndex + 1; i <= endIndex; i++) {
+    if (pivot > arr[i]) {
+      pvtIdx++;
+      swap(arr, pvtIdx, i);
     }
   }
-  swap(arr, startIndex, pvtIdx)
-  return pvtIdx
+  swap(arr, startIndex, pvtIdx);
+  return pvtIdx;
 }
 
-function quickSort(arr, left = 0, right = arr.length-1){
-  if(left < right){
-    let pvtIdx = partition(arr, left, right)
-  
-    quickSort(arr, left, pvtIdx - 1)
-    quickSort(arr, pvtIdx + 1, right)
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pvtIdx = partition(arr, left, right);
+
+    quickSort(arr, left, pvtIdx - 1);
+    quickSort(arr, pvtIdx + 1, right);
   }
-  return arr
+  return arr;
 }
 
+// with Comparator
+function pivot(arr, comp, startIndex = 0, endIndex = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let pivot = arr[startIndex],
+    pvtIdx = startIndex;
+
+  for (let i = startIndex + 1; i <= endIndex; i++) {
+    if (comp(pivot, arr[i]) >= 0) {
+      pvtIdx++;
+      swap(arr, pvtIdx, i);
+    }
+  }
+  swap(arr, startIndex, pvtIdx);
+  return pvtIdx;
+}
+
+function quickSort(arr, comp, left = 0, right = arr.length - 1) {
+  if (!comp) comp = (a, b) => a - b;
+  if (left < right) {
+    let pvtIdx = pivot(arr, comp, left, right);
+
+    quickSort(arr, comp, left, pvtIdx - 1);
+    quickSort(arr, comp, pvtIdx + 1, right);
+  }
+  return arr;
+}
--- Javascript_DSALGO/Sort Algorithms/RadixSort.js
+++ Javascript_DSALGO/Sort Algorithms/RadixSort.js
@@ -1,23 +1,43 @@
-// Radix Sort
+const getDigit = (num, pos) =>
+  Math.floor((Math.abs(num) / Math.pow(10, pos)) % 10);
+const digitCount = (num) =>
+  num == 0 ? 1 : Math.floor(Math.log10(Math.abs(num)) + 1);
+const mostDigits = (nums) => {
+  let cnt = 0;
+  for (let i = 0; i < nums.length; i++)
+    cnt = Math.max(digitCount(nums[i]), cnt);
+  return cnt;
+};
 
-function radixSort(arr){
-    const getDigitAt = (num, pos) => Math.floor(Math.abs(num) / Math.pow(10, pos) % 10)
-    const countDigits = (num) => num == 0 ? 1 : Math.floor(Math.log10(Math.abs(num)) + 1)
-    const maxDigitCount = (nums) => {
-        let cnt = 0
-        for(let i = 0; i < nums.length; i++) cnt = Math.max(countDigits(nums[i]), cnt)
-        return cnt
+function radixSort(nums) {
+  // Separate non-negative and negative numbers
+  let nonNegatives = nums.filter((num) => num >= 0);
+  let negatives = nums.filter((num) => num < 0);
+
+  // Sort non-negative numbers using radix sort
+  let maxDigitCnt = mostDigits(nonNegatives);
+  for (let k = 0; k < maxDigitCnt; k++) {
+    let buckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nonNegatives.length; i++) {
+      let bucketIdx = getDigit(nonNegatives[i], k);
+      buckets[bucketIdx].push(nonNegatives[i]);
     }
+    nonNegatives = [].concat(...buckets);
+  }
 
-    let maxDigitCnt = maxDigitCount(arr)
-    for(let k = 0; k < maxDigitCnt; k++){
-        let buckets = Array.from({length: 10}, () => [])
-        for(let i = 0; i < arr.length; i++){
-            let bucketIdx = getDigitAt(arr[i], k)
-            buckets[bucketIdx].push(arr[i])
-        }
-        nums = [].concat(...buckets)
+  // Sort negative numbers in reverse using radix sort
+  maxDigitCnt = mostDigits(negatives);
+  for (let k = 0; k < maxDigitCnt; k++) {
+    let buckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < negatives.length; i++) {
+      let bucketIdx = getDigit(Math.abs(negatives[i]), k);
+      buckets[bucketIdx].push(negatives[i]);
     }
+    negatives = [].concat(...buckets);
+  }
 
-    return nums
+  // Reverse the order of negative numbers and combine with non-negative numbers
+  return negatives.reverse().concat(nonNegatives);
 }
+
+console.log(radixSort([23, 6, -4, 95, 56, -69]));
